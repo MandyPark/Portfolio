b94ce618c7182700d0d941cbaf17c7ae03663475
Hyejin Park <mandy90909@gmail.com>
2018-11-13 15:25:43 +0900

    포트폴리오
    포트폴리오 사이트 추가

---
+++ js/main.js
@@ -0,0 +1,89 @@
+$(function(){
+	// 미리보기 그림 숨기기
+	$(".pfImg").hide();
+	$(".idx0").show();
+
+	// 윈도우 세로값
+	var ht = $(window).height()/2;
+	
+	// 커서 위치에 따라 흔들거리는 미리보기메뉴
+	$(window).on("mousemove",function(){
+		var x = event.pageX;
+		var y = event.pageY;
+		if (y<ht){
+			var ny = ht - y;
+			$("#pfList").stop().css({"transform":"skewY("+-5+"deg)"});
+			/*,"margin-top":"+="+10*/
+		} else {
+			var ny = y - ht;
+			$("#pfList").stop().css({"transform":"skewY("+5+"deg)"});
+			/*,"margin-top":"-="+10*/
+		}
+
+		// 미리보기메뉴에 마우스오버하면 왼쪽으로 나오기
+		$("#pfList li").mouseenter(function(){
+			$("#pfList li").css({"margin-left":0});
+			$(this).css({"margin-left":-20});
+		});
+		$("#pfList li").stop().mouseleave(function(){
+			$("#pfList li").css({"margin-left":0});
+		});
+
+		// Web site
+		$(".idx0 li").eq(0).css({"top":-117+y/120,"right":65.5+x/900+"%"});
+		$(".idx0 li").eq(1).css({"top":23-y/150,"right":45.8+x/1200+"%"});
+		$("#pfList li").eq(0).mouseenter(function(){
+			$(".pfImg").stop().fadeOut(300);
+			$(".idx0").stop().fadeIn(2000);
+		});		
+		// Promotion
+		$(".idx1 li").eq(0).css({"bottom":-228+y/120,"right":85.1+x/900+"%"});
+		$(".idx1 li").eq(1).css({"bottom":-228-y/150,"right":80.1+x/1200+"%"});
+		$(".idx1 li").eq(2).css({"bottom":-228-y/100,"right":71.2-x/1500+"%"});
+		$(".idx1 li").eq(3).css({"bottom":-228+y/90,"right":67.8-x/900+"%"});
+		$(".idx1 li").eq(4).css({"bottom":-228-y/180,"right":62-x/1500+"%"});
+		$(".idx1 li").eq(5).css({"bottom":-228-y/80,"right":55.8-x/800+"%"});
+		$("#pfList li").eq(1).mouseenter(function(){
+			$(".pfImg").stop().fadeOut(300);
+			$(".idx1").stop().fadeIn(2000);		
+		});
+		// Typography
+		$(".idx2 li").eq(0).css({"top":-60+y/120,"right":50.6-x/900+"%"});
+		$(".idx2 li").eq(1).css({"top":64-y/100,"right":70.6+x/500+"%"});
+		$("#pfList li").eq(2).mouseenter(function(){
+			$(".pfImg").stop().fadeOut(300);
+			$(".idx2").stop().fadeIn(2000);
+		});
+		// Emblem
+		$(".idx3 li").eq(0).css({"top":-48+y/60,"right":82.5+x/900+"%"});
+		$(".idx3 li").eq(1).css({"top":-50-y/50,"right":67.1+x/800+"%"});
+		$(".idx3 li").eq(2).css({"top":3+7-y/50,"right":52-x/700+"%"});
+		$(".idx3 li").eq(3).css({"top":180+y/90,"right":74.7-x/900+"%"});
+		$(".idx3 li").eq(4).css({"top":116-y/80,"right":58.3+x/1000+"%"});
+		$("#pfList li").eq(3).mouseenter(function(){
+			$(".pfImg").stop().fadeOut(300);
+			$(".idx3").stop().fadeIn(2000);
+		});
+		// Poster
+		$(".idx4 li").eq(0).css({"top":-140+y/60,"right":74.75+x/900+"%"});
+		$(".idx4 li").eq(1).css({"top":-40-y/50,"right":52.75+x/800+"%"});
+		$("#pfList li").eq(4).mouseenter(function(){
+			$(".pfImg").stop().fadeOut(300);
+			$(".idx4").stop().fadeIn(2000);
+		});
+		// Magazine
+		$(".idx5 li").eq(0).css({"top":-100+y/60,"right":50.8+x/800+"%"});
+		$("#pfList li").eq(5).mouseenter(function(){
+			$(".pfImg").stop().fadeOut(300);
+			$(".idx5").stop().fadeIn(2000);		
+		});
+		// Banner
+		$(".idx6 li").eq(0).css({"top":-120+y/80,"right":68.25+x/900+"%"});
+		$(".idx6 li").eq(1).css({"top":-20-y/100,"right":43.75+x/800+"%"});
+		$(".idx6 li").eq(2).css({"top":125-y/90,"right":71.5-x/900+"%"});
+		$("#pfList li").eq(6).mouseenter(function(){
+			$(".pfImg").stop().fadeOut(300);
+			$(".idx6").stop().fadeIn(2000);
+		});
+	});
+});
---
+++ js/works.js
@@ -0,0 +1,67 @@
+$(function(){
+
+	//리사이즈
+	var worksWh = $(window).width() - 288;
+	$(".wrap").css({"width":worksWh});
+	$(window).resize(function(){
+		var worksWh = $(window).width() - 288;
+		$(".wrap").css({"width":worksWh});
+	});
+
+	// 이미지 중앙정렬
+	// var imgWt = $(".workImg img").width();
+	// var imgHt = $(".workImg img").height();
+	// $(".workImg img").css({"width":imgWt,"height":imgHt});
+	
+
+	// next + prev 오버
+	$(".prev").mouseenter(function(){
+		$("#prev").attr("src","images/btn_prevhover.png");
+	});
+	$(".prev").mouseleave(function(){
+		$("#prev").attr("src","images/btn_prev.png");
+	});
+	$(".next").mouseenter(function(){
+		$("#next").attr("src","images/btn_nexthover.png");
+	});
+	$(".next").mouseleave(function(){
+		$("#next").attr("src","images/btn_next.png");
+	});
+	
+	// 페이지 기본 표기
+	var pageNum = $(".workImg li").index()+1;
+	$("p.now").text(pageNum);
+	var pageTotal = $(".workImg li").last().index()+1;
+	$("p.total").text(pageTotal);
+
+	// next 버튼 누르면 다음 컨텐츠로
+	$("#next").click(function(){
+		// 다음 번호로 바뀜
+		pageNum += 1;
+		if(pageNum>pageTotal){ // 마지막페이지를 더 안넘어가도록
+			pageNum = pageTotal;
+		} else {
+			// 다음 그림과 내용 + 인덱스로 바뀜
+			$(".workImg li").first().appendTo(".workImg");
+			$(".explain li").first().appendTo(".explain");
+			$("p.now").text(pageNum);
+		}
+		console.log(pageNum);
+	});
+
+	// prev 버튼 누르면 이전 컨텐츠로
+	$("#prev").click(function(){
+		// 이전 번호로 바뀜
+		pageNum -= 1;
+		if(pageNum<=0){ // 첫페이지를 더 안넘어가도록
+			pageNum = 1;
+		} else {
+			// 이전 그림과 내용 + 인덱스로 바뀜
+			$(".workImg li").last().prependTo(".workImg");
+			$(".explain li").last().prependTo(".explain");
+			$("p.now").text(pageNum);
+		}
+		console.log(pageNum);
+	});
+
+});
