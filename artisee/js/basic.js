a5a9fa10d2ab52af7e83708944bda30fce05e4c8
Hyejin Park <44991391+MandyPark@users.noreply.github.com>
2018-12-03 04:07:23 +0900

    아티제

---
+++ artisee/js/basic.js
@@ -0,0 +1,9 @@
+setInterval(function(){
+	$(".lights li:nth-child(4)").toggleClass("opc5");
+},4000);
+setInterval(function(){
+	$(".lights li:nth-child(3)").toggleClass("opc0");
+},3000);
+setInterval(function(){
+	$(".lights li:nth-child(2)").toggleClass("opc10");
+},2500);
